d93c44baa17dd9b867196f293e540dadca430374
majekbezjajek <tpkorepetycje@gmail.com>
2026-07-08 15:00:13 +0200

    Obsługa zmiennej PORT dla wdrożenia w chmurze

--- app.py
+++ app.py
@@ -481,5 +481,8 @@ def check_continuous():
     return check_answer(QUESTIONS_CONTINUOUS, request.get_json())
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
